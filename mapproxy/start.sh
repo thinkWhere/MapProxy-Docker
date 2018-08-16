eae4b66b4a70efbdcca88354d36ba02987076942
Iain Hunter <iain.hunter@thinkwhere.com>
2018-08-16 17:34:09 +0100

    Test privs

--- mapproxy/start.sh
+++ mapproxy/start.sh
@@ -9,6 +9,9 @@ useradd --shell /bin/bash --uid $USER_ID --gid $GROUP_ID $USER_NAME
 # MapProxy config, and the app.py will be provided in the target machine via EFS
 # TODO initialise the MapProxy config from git in the event of a completely new environment
 
+# Ensure www-data user has privs to access mapproxy dir
+chgrp www-data mapproxy
+
 # Run MapProxy using uwsgi/nginx
 su $USER_NAME -c "uwsgi --ini /uwsgi.ini"
 
